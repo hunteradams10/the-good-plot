b01d794cab5802e6ba960e895487fa1c3d10c0e8
Hunter Adams <hunteradams10@gmail.com>
2022-06-27 16:43:12 +1000

    added condition enum

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,13 @@ class Listing < ApplicationRecord
   belongs_to :genre
   belongs_to :user
   has_rich_text :summary
+
+  #enum allows us to specify how to conditions are displayed in the browser
+
+  enum condition: {
+    needs_rescue: 1,
+    pages_missing: 2,
+    well_loved: 3,
+    brand_new: 4
+  }
 end
