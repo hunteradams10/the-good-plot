c4bb2d7b09d03d705253291c8b652c9cc1808b6d
Hunter Adams <hunteradams10@gmail.com>
2022-06-28 16:48:33 +1000

    orders table and relationships

--- db/migrate/20220628064000_create_orders.rb
+++ db/migrate/20220628064000_create_orders.rb
@@ -3,8 +3,8 @@ class CreateOrders < ActiveRecord::Migration[6.1]
     create_table :orders do |t|
       t.serial :order
       t.references :listing, null: false, foreign_key: true
-      t.references :buyer, null: false, foreign_key: true
-      t.references :seller, null: false, foreign_key: true
+      t.references :buyer, null: false, foreign_key: { to_table: :users }
+      t.references :seller, null: false, foreign_key: { to_table: :users }
       t.references :address, null: false, foreign_key: true
 
       t.timestamps
